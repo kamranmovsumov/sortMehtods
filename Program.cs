3d030cb0d3af7c2fbf09f8e9af122308dc5e2527
kamran.movsumov <kamran.movsumov@outlook.com>
2022-10-27 22:40:17 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace sortMehtods
+{
+    internal class Program
+    {
+        static void MyArrItem(int[] myArr)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                int arrRand = rand.Next(0, 100);
+                myArr[i] = arrRand;
+            }
+        }
+
+        static int FindMax(int[] myArr)
+        {
+            int max = myArr[0];
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                if (max < myArr[i])
+                {
+                    max = myArr[i];
+                }
+            }
+            return max;
+        }
+        static void ShowArr(int[] myArr)
+        {
+            foreach (int item in myArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void CountingSort(int[] myArr)
+        {
+            int max = FindMax(myArr);
+            int[] tempArr = new int[max + 1];
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                for (int j = 0; j < tempArr.Length; j++)
+                {
+                    if (j == myArr[i])
+                    {
+                        tempArr[j]++;
+                    }
+                }
+            }
+            for (int i = 0, j = 0; i < tempArr.Length; i++)
+            {
+                if (tempArr[i] != 0)
+                {
+                    myArr[j] = i;
+                    j++;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] myArr = new int[10];
+            MyArrItem(myArr);
+            ShowArr(myArr);
+            CountingSort(myArr);
+            Console.WriteLine();
+            ShowArr(myArr);
+        }
+    }
+}
