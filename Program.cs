f36897c32566b6c37a2809fd1f277ab7c76b45c6
Kamran Movsumov <95227478+kamranmovsumov@users.noreply.github.com>
2022-11-09 21:20:34 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -109,9 +109,9 @@ namespace myFirstApp
             for (int i = 0; i < myArr.Length; i++)
             {
                 int index = i;
-                for (int j = i-1; j >=0; j--)
+                for (int j = i - 1; j >= 0; j--)
                 {
-                    if (myArr[index]<myArr[j])
+                    if (myArr[index] < myArr[j])
                     {
                         int temp = myArr[index];
                         myArr[index] = myArr[j];
@@ -128,11 +128,135 @@ namespace myFirstApp
 
         static void QuickSort(int[] myArr)
         {
-
+            QuickSort(myArr, 0, myArr.Length - 1);
+        }
+        #region QuickSort
+        static void QuickSort(int[] myArr, int low, int high)
+        {
+            if (low >= high)
+                return;
+            int pivot = myArr[high];
+            int pivotNewIndex = Partition(myArr, low, high, pivot);
+            QuickSort(myArr, low, pivotNewIndex - 1);
+            QuickSort(myArr, pivotNewIndex + 1, high);
         }
 
+        static int Partition(int[] myArr, int low, int high, int pivot)
+        {
+            int j = low;
+            for (int i = low; i <= high; i++)
+            {
+                if (myArr[i] <= pivot)
+                {
+                    int temp = myArr[i];
+                    myArr[i] = myArr[j];
+                    myArr[j] = temp;
+                    j++;
+                }
+            }
+            return j - 1;
+        }
+        #endregion
 
+        #region MergeSort
+        static void MergeSort(int[] myArr)
+        {
+            MergeSort(myArr, 0, myArr.Length - 1);
+        }
 
+        static void MergeSort(int[] myArr, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            int middle = (left + right) / 2;
+
+            MergeSort(myArr, left, middle);
+            MergeSort(myArr, middle + 1, right);
+
+            Merge(myArr, left, middle, right);
+        }
+
+        static void Merge(int[] myArr, int left, int middle, int right)
+        {
+            int[] leftArray = new int[middle - left + 1];
+            int[] rightArray = new int[right - middle];
+
+            int leftIndex = 0, rightIndex = 0;
+
+            for (int i = left; i <= middle; i++)
+            {
+                leftArray[leftIndex] = myArr[i];
+
+                leftIndex++;
+            }
+
+            for (int i = middle + 1; i <= right; i++)
+            {
+                rightArray[rightIndex] = myArr[i];
+
+                rightIndex++;
+            }
+
+            int mainIndex = left;
+            leftIndex = 0;
+            rightIndex = 0;
+
+            while (leftIndex < leftArray.Length && rightIndex < rightArray.Length)
+            {
+                if (leftArray[leftIndex] < rightArray[rightIndex])
+                {
+                    myArr[mainIndex] = leftArray[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    myArr[mainIndex] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+
+                mainIndex++;
+            }
+
+            while (leftIndex < leftArray.Length)
+            {
+                myArr[mainIndex] = leftArray[leftIndex];
+
+                mainIndex++;
+                leftIndex++;
+            }
+
+            while (rightIndex < rightArray.Length)
+            {
+                myArr[mainIndex] = rightArray[rightIndex];
+
+                mainIndex++;
+                rightIndex++;
+            }
+        }
+        #endregion
+
+        #region Binary Search
+        static int BinarySearch(int[] myArr, int element)
+        {
+            return BinarySearch(myArr, element, 0, myArr.Length - 1);
+        }
+
+        static int BinarySearch(int[] myArr, int element, int left, int right)
+        {
+            if (left > right)
+                return -1;
+            int middle = (left + right) / 2;
+
+            if (myArr[middle] > element)
+                return BinarySearch(myArr, element, left, middle - 1);
+
+            if (myArr[middle] < element)
+                return BinarySearch(myArr, element, middle + 1, right);
+
+            return middle;
+        }
+        #endregion
         static void Main(string[] args)
         {
             int[] myArr = new int[10];
@@ -142,7 +266,20 @@ namespace myFirstApp
             //SelectionSort(myArr);
             //BubbleSort(myArr);
             //InsertionSort(myArr);
-            QuickSort(myArr);
+            //QuickSort(myArr);
+            //MergeSort(myArr);
+            #region BinarySearch
+            int element = Convert.ToInt32(Console.ReadLine());
+            int index = BinarySearch(myArr, element);
+            if (index != -1)
+            {
+                Console.WriteLine($"Element found. Index: {index}");
+            }
+            else
+            {
+                Console.WriteLine("Element not found");
+            }
+            #endregion
             Console.WriteLine();
             ShowArr(myArr);
         }
